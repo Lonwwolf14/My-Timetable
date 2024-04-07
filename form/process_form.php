6057e9f7e43532bb17ce28036212455f7cd8dcb6
Vortex <parabraviraj142730@gmail.com>
2024-04-07 17:30:45 +0530

    Database connectivity

--- form/process_form.php
+++ form/process_form.php
@@ -1,20 +1,25 @@
 <?php
-// Retrieve form data
-$first_name = $_POST['first-name'];
-$last_name = $_POST['last-name'];
-$address = $_POST['address'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$gender = $_POST['gender'];
-$conn = new mysqli('localhost', 'root', '', 'form');
-if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+if(isset($_POST['first_name'], $_POST['last_name'], $_POST['address'], $_POST['email'], $_POST['phone'], $_POST['gender'])) {
+    // Retrieve form data
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $gender = $_POST['gender'];
+
+    $conn = new mysqli('localhost', 'root', '', 'form');
+    if ($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO new (first_name, last_name, address, email, phone, gender) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $first_name, $last_name, $address, $email, $phone, $gender);
+        $stmt->execute();
+        echo "<strong>Welcome, $first_name!</strong>";
+        $stmt->close();
+        $conn->close();
+    }
 } else {
-    $stmt = $conn->prepare("INSERT INTO form_details (first_name, last_name, address, email, phone, gender) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $first_name, $last_name, $address, $email, $phone, $gender);
-    $stmt->execute();
-    echo "Registration successful....";
-    $stmt->close();
-    $conn->close();
+    echo "All fields are required!";
 }
 ?>
